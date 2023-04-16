463d2b56dfdef1f9486124579f2b476f6d5975ce
berbu25 <sergio.berbesi@udea.edu.co>
2023-04-16 18:59:46 -0500

    Creación de programa 1

---
+++ Programa 1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+void ConvertirAChar();
+void FiltroNiveles();
+void MostrarMaterias();
+void MostrarHorarios();
+void VerificacionesMatricular();
+void CantidadDeCreditos();
+void AsignacionHorasExtras();
+
+void main (){
+    
+}
+return 0;
